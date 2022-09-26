5d6bddd9018c2c09c0dcc8e7a604bffeaa41b17d
Rodrigo <pinto_enam@hotmail.com>
2022-09-26 20:08:23 -0300

    avances en la parte de javascript y css

---
+++ horca.js
@@ -0,0 +1,10 @@
+let palabras = ["ALURA", "ORACLE", "JAVASCRIPT", "HTML"];
+
+let tablero = document.getElementById("forca").getContext("2d");
+
+let palabraSecreta=""
+
+function iniciarJuego(){
+    document.getElementById("iniciar-juego").style.display ="none";
+
+}
